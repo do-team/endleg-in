fcbb61c20e65e51e68f6b8944db72b98c4fe0484
kubedav <davesade.42@gmail.com>
2017-02-01 11:21:52 +0100

    Push to Lambda test

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ exports.handler = (event, context, callback) => {
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
     callback(null, event.key1);  // Echo back the first key value
-    //callback('Something went wrong');
+    //callback('Something went wrong, but like really really wrong.');
 };
