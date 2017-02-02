79314b4ca3e466afe524ed632bd000ecf40b22c6
kubedav <davesade.42@gmail.com>
2017-02-02 15:12:24 +0100

    Push to Lambda test

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 
 console.log('Loading function');
 
-exports.handler = (event, context, callback) => {
+/*
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
     callback(null, event.key1);  // Echo back the first key value
     //callback('Something went wrong, but like really really wrong.');
+*/
+
+exports.handler = (event, context, callback) => {
+for (int j = 0; j < 10; j++)
+{
+  PutRecordRequest putRecordRequest = new PutRecordRequest();
+  putRecordRequest.setStreamName( "endleg-test" );
+  putRecordRequest.setData(ByteBuffer.wrap( String.format( "testData-%d", j ).getBytes() ));
+  putRecordRequest.setPartitionKey( String.format( "partitionKey-%d", j/5 ));
+  putRecordRequest.setSequenceNumberForOrdering( j-1 );
+  PutRecordResult putRecordResult = client.putRecord( putRecordRequest );
+  sequenceNumberOfPreviousRecord = putRecordResult.getSequenceNumber();
+}
 };
