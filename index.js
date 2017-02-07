ecd54faf6902fb2d7acaa9c3a287c20faef3a1bb
David Kubec <davesade.42@gmail.co,m>
2017-02-07 21:40:34 +0100

    Tutorial copy paste, adopted to our table.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 var AWS = require('aws-sdk');
 
-'use strict';
-
-console.log('Loading function');
-
-/*
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
-    console.log('value3 =', event.key3);
-    callback(null, event.key1);  // Echo back the first key value
-    //callback('Something went wrong, but like really really wrong.');
-*/
-
 exports.handler = (event, context, callback) => {
-for (int j = 0; j < 10; j++)
-{
-  PutRecordRequest putRecordRequest = new PutRecordRequest();
-  putRecordRequest.setStreamName( "endleg-test" );
-  putRecordRequest.setData(ByteBuffer.wrap( String.format( "testData-%d", j ).getBytes() ));
-  putRecordRequest.setPartitionKey( String.format( "partitionKey-%d", j/5 ));
-  putRecordRequest.setSequenceNumberForOrdering( j-1 );
-  PutRecordResult putRecordResult = client.putRecord( putRecordRequest );
-  sequenceNumberOfPreviousRecord = putRecordResult.getSequenceNumber();
-}
+
+AWS.config.update({
+  region: "eu-central-1",
+  endpoint: "https://dynamodb.eu-central-1:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "endleg-main";
+var user = "test";
+var name = "Dave";
+
+var params = {
+    TableName:table,
+    Item:{
+        "user": user,
+        "name": name
+    }
 };
+
+console.log("Adding a new item...");
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
+
+}
