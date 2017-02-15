d078fc2afb6259eb0d0219e7ee5dfed459a73255
kubedav <davesade.42@gmail.com>
2017-02-15 09:21:35 +0100

    Pushing to Lambda!

--- CreateItem.js
+++ CreateItem.js
@@ -1,13 +1,14 @@
 var AWS = require("aws-sdk");
 var fs = require('fs');
 
+exports.handler = (event, context, callback) => {
+
 AWS.config.update({
   region: "eu-central-1",
-  endpoint: "http://localhost:8000"
+  endpoint: "dynamodb.eu-central-1.amazonaws.com"
 });
 
 var dynamodb = new AWS.DynamoDB();
-
 
 var incoming = JSON.parse(fs.readFileSync('testevent.json', 'utf-8'));
 console.log(incoming);
@@ -17,25 +18,24 @@ var params = {
         Item: {
             "user": incoming.user,
             "name": incoming.name,
-            "card1":  incoming.card1,
+            "card1": incoming.card1,
+            "card2": incoming.card2,
+            "card3": incoming.card3,
+            "card4": incoming.card4,
+            "card5": incoming.card5,
             "fighflag": incoming.fightflag
         }
     };
-
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 docClient.put(params, function(err, data) {
        if (err) {
            console.log(err);
-           console.error("Unable to add user", incoming.user, ". Error JSON:", JSON.stringify(err, null, 2));
+           console.error("Unable to add new items from user ", incoming.user, ". Error JSON:", JSON.stringify(err, null, 2));
        } else {
-           console.log("PutItem succeeded, user", incoming.user, " added.");
+           console.log("Request by user", incoming.user, " was successfully added.");
        }
     });
 
-
-
-
-
-
+};
--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
+var fs = require('fs');
 
 exports.handler = (event, context, callback) => {
 
 AWS.config.update({
   region: "eu-central-1",
-  endpoint: "http://localhost:8000"
+  endpoint: "dynamodb.eu-central-1.amazonaws.com"
 });
+
+var dynamodb = new AWS.DynamoDB();
+
+var incoming = JSON.parse(fs.readFileSync('testevent.json', 'utf-8'));
+console.log(incoming);
+
+var params = {
+        TableName: "users",
+        Item: {
+            "user": incoming.user,
+            "name": incoming.name,
+            "card1": incoming.card1,
+            "card2": incoming.card2,
+            "card3": incoming.card3,
+            "card4": incoming.card4,
+            "card5": incoming.card5,
+            "fighflag": incoming.fightflag
+        }
+    };
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-var table = "users";
-var user = "davesade";
-var card1 = "A";
-var card2 = "B";
-var card3 = "C";
-var card4 = "D";
-var card5 = "E";
-var name = "dave";
-var flag = 1;
-
-var params = {
-    TableName:table,
-    Item:{
-        "user": user,
-        "name": name,
-        "fightflag": flag,
-        "card1": card1,
-        "card2": card2,
-        "card3": card3,
-        "card4": card4,
-        "card5": card5,
-    }
-};
-console.log("This is event: " + event);
-console.log("Adding a new item...");
 docClient.put(params, function(err, data) {
-    if (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Added item:", JSON.stringify(data, null, 2));
-    }
-});
+       if (err) {
+           console.log(err);
+           console.error("Unable to add new items from user ", incoming.user, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("Request by user", incoming.user, " was successfully added.");
+       }
+    });
 
 };
